3cb1855b3c0b0938abb4c612fab37da9063c0f95
christina.beckford <christina.beckford@gmail.com>
2020-03-27 20:37:52 -0400

    Created the notification model which consist of the sensor and the fire hazard. Interface for the notification.

---
+++ Notification/INotification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification
+{
+    public interface INotification
+    {
+         void LightPanel();
+        void TurnOnSound();
+
+        string userCredentials();
+
+        int resetSensor();
+
+        void notifyEmergencyServices();
+
+        void notifyMonitoringServices();
+
+        void LogTable(); 
+
+    }
+}
---
+++ Notification/Sensor_FireHazardModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification
+{
+    public class Sensor_FireHazardModel
+    {
+        //Sensors properties
+        public int sensor1 { get; set; }
+        public int sensor2 { get; set; }
+
+        public int sensor3 { get; set; }
+
+        public int sensor4 { get; set; }
+
+
+        //Fire Harzard properties for monitor panel 
+        public string Smoke { get; set; }
+        public string Fire { get; set; }
+
+        public string Heat { get; set; }
+
+
+        //Fire Hazard properties for analyzing
+
+        public int fire { get; set; }
+        public int smoke { get; set; }
+        public int  heat { get; set; }  
+
+
+    }
+}

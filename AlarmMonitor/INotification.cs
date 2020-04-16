71fa8978047923049998f16723eb68fe82abdc15
christina.beckford <christina.beckford@gmail.com>
2020-04-15 23:11:40 -0400

    Copy logic from Christina's branch to the master branch

---
+++ AlarmMonitor/INotification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmMonitor
+{
+    public interface INotification
+    {
+    
+
+
+
+            void userCredentials();
+
+            int Resetsensor();
+
+            void notifyEmergencyServices();
+
+            void notifyMonitoringServices();
+
+            void LogTable();
+
+            void insertSecuritypin();
+        
+    }
+}
